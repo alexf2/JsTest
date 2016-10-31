1429e03b4e0e0f4ea28f4383e730a18ba0a2ece4
Alexey Fedorov <Aleksey-Fedorov@mailfrom.ru>
2016-11-01 01:03:11 +0300

    Adding WebPack tests

---
+++ WebPackDynamic/webpack.config.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + "/src",
+
+    entry: {
+        home:  "./home",
+    },
+
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].js",
+        library: "[name]",
+	      publicPath: '/dist/'
+    },
+
+    devtool: "source-map",
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({NODE_ENV: JSON.stringify(NODE_ENV), USER: JSON.stringify(process.env.USER)})
+    ],
+
+    resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions:         ['', '.js']
+  },
+
+  resolveLoader: {
+    modulesDirectories: ['node_modules'],
+    moduleTemplates:    ['*-loader', '*'],
+    extensions:         ['', '.js']
+  },
+
+  module: {
+
+    loaders: [{
+      test:   /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+
+      query: {
+                presets: ['es2015'],
+                plugins: ['transform-runtime']
+            }
+    }]
+
+  }
+}
+
+if (NODE_ENV == 'production') {
+  module.exports.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          // don't show unreachable variables etc
+          warnings:     false,
+          drop_console: true,
+          unsafe:       true
+        }
+      })
+  );
+}
