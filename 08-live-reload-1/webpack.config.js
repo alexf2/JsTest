0c92416ac3a3ce9c6a413e718401196c6ce8e526
Alexey Fedorov <Alexey.Fedorov@confirmit.com>
2016-11-03 16:43:28 +0100

    Dynamic 5

--- 08-live-reload-1/webpack.config.js
+++ 08-live-reload-1/webpack.config.js
@@ -7,7 +7,7 @@ const rimraf = require('rimraf');
 
 module.exports = {
   context: path.resolve(__dirname, "frontend"),
-  entry:   {
+  entry:   { //--inline, --hot
     main: ['./main']
   },
   output:  {
@@ -17,38 +17,26 @@ module.exports = {
   },
 
   module: {
-
     loaders: [{
-      test:   /\.js$/,
-      include: __dirname + '/frontend',
-      loader: "babel-loader",
-
-      query: {
-                presets: ['es2015'],
-                plugins: ['transform-runtime']
-            }
-    }, 
-    
-    {
+      test:    /\.js$/,
+      include: path.resolve(__dirname, "frontend"),
+      loader:  "babel?presets[]=es2015"
+    }, {
       test:   /\.jade$/,
       loader: "jade"
-    }, 
-    
-    {
+    }, {
       test:   /\.styl$/,
       loader: ExtractTextPlugin.extract('style', 'css!stylus?resolve url')
-    }, 
-    
-    {
+    }, {
       test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-      loader: 'file?name=[name].[ext]?[hash]'
+      loader: 'file?name=[path][name].[ext]?[hash]'
     }]
   },
 
   plugins: [
     //{apply: (compiler) => {rimraf.sync(compiler.options.output.path);} },
-    new ExtractTextPlugin('[name].css', {allChunks: true, disable: process.env.NODE_ENV=='development'})
-    //new webpack.HotModuleReplacementPlugin()
+    new ExtractTextPlugin('[name].css', {allChunks: true, disable: process.env.NODE_ENV=='development'}),
+    new webpack.HotModuleReplacementPlugin()
   ],
 
   devServer: {
@@ -56,7 +44,7 @@ module.exports = {
     port: 8080, // default
 
     contentBase: path.resolve(__dirname, "backend"), //static content
-    //hot: true
+    hot: true
 
     /*proxy: [
       {
